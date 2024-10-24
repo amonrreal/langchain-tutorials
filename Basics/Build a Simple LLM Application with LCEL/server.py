b328ac6a0d14a4bade491a6fc20714de604af241
Alvaro M <alvaro.monrreal@gmail.com>
2024-10-24 12:16:04 -0600

    Refactor after running the code and removing unnecessary parts

--- Basics/Build a Simple LLM Application with LCEL/server.py
+++ Basics/Build a Simple LLM Application with LCEL/server.py
@@ -12,9 +12,6 @@ from langserve import add_routes
 
 load_dotenv()
 
-os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGCHAIN_TRACING_V2")
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 system_template = "Translate the following into {language}:"
