e557d99f6c6a223f547f6aa150b2dd37b99c55fa
Alvaro M <alvaro.monrreal@gmail.com>
2024-10-24 02:52:19 -0600

    Start Langchain Tutorial

---
+++ Basics/Build a Simple LLM Application with LCEL/server.py
@@ -0,0 +1,50 @@
+# Serving with LangServe
+# For more information, visit:
+# https://python.langchain.com/docs/tutorials/llm_chain/#serving-with-langserve
+
+# pip install "langserve[all]"
+
+#!/usr/bin/env python
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langserve import add_routes
+
+load_dotenv()
+
+os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGCHAIN_TRACING_V2")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+
+
+model = ChatOpenAI()
+parser = StrOutputParser()
+
+chain = prompt_template | model | parser
+
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
